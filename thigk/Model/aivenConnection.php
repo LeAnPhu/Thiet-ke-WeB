750289fd17fbdccead3b0ae848a523a0a9775302
LeAnPhu <155645777+LeAnPhu@users.noreply.github.com>
2024-05-13 07:21:09 +0700

    Add files via upload

---
+++ thigk/Model/aivenConnection.php
@@ -0,0 +1,39 @@
+<?php
+
+$uri = "mysql://avnadmin:AVNS_UwhhTGwXnNdIitOUzGp@mysql-32b94e72-st-cdc3.l.aivencloud.com:12738/defaultdb?ssl-mode=REQUIRED";
+
+$fields = parse_url($uri);
+
+// build the DSN including SSL settings
+$conn = "mysql:";
+$conn .= "host=" . $fields["host"];
+$conn .= ";port=" . $fields["port"];;
+$conn .= ";dbname=defaultdb";
+$conn .= ";sslmode=verify-ca;sslrootcert='pri/ca.pem'";
+
+try {
+    $db = new PDO($conn, $fields["user"], $fields["pass"]);
+
+    $stmt = $db->query("SELECT version()");
+
+    print($stmt->fetch()[0]);
+    $row = $db->query("select * from User");
+
+    /*printf($row->rowCount());
+    if($row->rowCount()>0){
+        while ($r = $row->fetch()[2]){
+            echo $r;
+
+        }
+    }*/
+    if($row->rowCount()>0){
+        while ($r = $row->fetchObject()){
+            echo $r->date;
+            echo $r->description;
+
+        }
+    }
+   
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
---
+++ thigk/Model/aivenMySQLi.php
@@ -0,0 +1,28 @@
+<?php
+echo "hello";
+phpinfo();
+
+$host="mysql-32b94e72-st-cdc3.l.aivencloud.com";
+
+$user="avnadmin";
+$port="12738";
+$password="AVNS_UwhhTGwXnNdIitOUzGp";
+
+$con=mysqli_connect($host,$user,$password);
+
+mysqli_select_db($con,"defaultdb"); 
+//To select the database
+
+session_start(); //To start the session
+
+$query=mysqli_query($con,"select * from sach"); 
+
+//$row = mysqli_fetch_array($query);
+
+$rowcount=mysqli_num_rows($query);
+//made query after establishing connection with database.
+
+//echo "my result <a href='data/" . htmlentities($row['classtype'], ENT_QUOTES, 'UTF-8') . ".php'>sinh vien</a>";
+
+printf($rowcount);
+?>
